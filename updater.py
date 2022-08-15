bf7396731e49b106029da309af04f7ffc2333fef
Omar Ahmed <101176394+Srd8@users.noreply.github.com>
2022-08-15 12:54:08 +0300

    Add files via upload

--- updater.py
+++ updater.py
@@ -58,14 +58,14 @@ def updater():
 @sudo_users_only
 async def update_repo(_, message: Message):
     chat_id = message.chat.id
-    msg = await message.reply("🔄 `تتم البرمجه بواسطة سورس ايثون......`")
+    msg = await message.reply("🔄 `تتلبرمجه بواسطـه سورس كيتو......`")
     update_avail = updater()
     if update_avail:
         await msg.edit("✅ update finished\n\n• bot restarted, back active again in 1 minutes.")
         system("git pull -f && pip3 install --no-cache-dir -r requirements.txt")
         execle(sys.executable, sys.executable, "main.py", environ)
         return
-    await msg.edit(f"تم الترقية بواسطه سورس [Ξ𝗜𝗧𝗛𝗢𝗡™ ايثون](https://t.me/EITHON1)", disable_web_page_preview=True)
+    await msg.edit(f"تم الترقيه بواسطه سورس كيتو ", disable_web_page_preview=True)
 
 
 @Client.on_message(command(["ريستارت", f"restart@{BOT_USERNAME}"]) & ~filters.edited)
